520cf5520fbbb9b0b5057d04bfc6fc71e1540107
jonatan-ordonezbu <jonatan.ordonez.burbano.-nd@disney.com>
2026-03-25 14:14:26 -0500

    fix: remove defaultPort from Stackblitz configuration and update server log message

--- server/src/app.ts
+++ server/src/app.ts
@@ -31,7 +31,7 @@ app.use(errorsMiddleware);
 const start = async () => {
   await initDb();
   app.listen(PORT, () => {
-    console.log('Server is running on http://localhost:' + PORT);
+    console.log('Server running on port ' + PORT);
   });
 };
 
