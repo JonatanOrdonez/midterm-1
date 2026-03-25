b8d75d9685c3db6e1810745ac06b09c515d14d2b
jonatan-ordonezbu <jonatan.ordonez.burbano.-nd@disney.com>
2026-03-25 14:16:25 -0500

    fix: comment out server log message for port

--- server/src/app.ts
+++ server/src/app.ts
@@ -31,7 +31,7 @@ app.use(errorsMiddleware);
 const start = async () => {
   await initDb();
   app.listen(PORT, () => {
-    console.log('Server running on port ' + PORT);
+    // console.log('Server running on port ' + PORT);
   });
 };
 
